1d406b7e56274a29bb79eee68995bda35ab3df70
Bears <evanbero1980@gmail.com>
2021-02-23 12:33:32 -0600

    added navbar with corresponding routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Playlist from './pages/Playlist';
 import Search from './pages/Search';
+import NavbarComp from './components/NavbarComp';
 
 function App() {
   return (
     <Router>
       <div>
+      <NavbarComp/>
         <Route path='/' exact component={Search} />
         <Route path='/playlist' exact component={Playlist} />
       </div>
